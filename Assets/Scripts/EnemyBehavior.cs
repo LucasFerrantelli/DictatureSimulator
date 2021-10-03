7c37dc040282551d60d18e1b8c0dcb667c09a82c
luko <lucas.ferrantelli@gmail.com>
2021-10-03 23:36:05 +0200

    added rain system / entities animations / set up some effect features

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    
+    public List<Material> variance;
+    Material mat;
 
     public float hp;
     public float movementspeed;
@@ -25,8 +26,24 @@ public class EnemyBehavior : MonoBehaviour
     {
         if(LawManager.Instance.allowSelfDefense)
         {
-            CameraHandler.Instance.remainingShakeDuration = 4;
-            TakeDamage(10);
+            //CameraHandler.Instance.remainingShakeDuration = 4;
+            //TakeDamage(10);
+            if(Random.Range(0,10) < 2)
+            {
+                ApplyEffect(AdditionalEffect.Poison);
+            }
+            else if (Random.Range(0, 10) < 6)
+            {
+                ApplyEffect(AdditionalEffect.Stun);
+            }
+            else if (Random.Range(0, 10) < 8)
+            {
+                TakeDamage(0.01f);
+            }
+            else
+            {
+                TakeDamage(200);
+            }
         }
     }
 
@@ -34,6 +51,7 @@ public class EnemyBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        mat = GetComponentInChildren<MeshRenderer>().material = variance[Random.Range(0, 3)];
         GameManager.Instance.currentEnemies.Add(this);
         movementspeed += speedAdditioner;
         hp += hpAdded;
@@ -45,10 +63,16 @@ public class EnemyBehavior : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        
         hp -= damage * GameManager.Instance.damageMultiplier;
-        if(hp <= 0)
+        
+        if (hp <= 0)
         {
             Die();
+        }
+        else
+        {
+            GetComponentInChildren<Animator>().Play("Hurt");
         }
     }
 
@@ -60,30 +84,43 @@ public class EnemyBehavior : MonoBehaviour
 
     public void ApplyEffect(AdditionalEffect effect)
     {
-        switch (effect)
+        if (isAlive)
         {
-            case AdditionalEffect.Slow:
-                slowPercent = GameManager.Instance.slowPercent;
-                slowRemainingDuration = GameManager.Instance.slowDuration;
+            switch (effect)
+            {
+                case AdditionalEffect.Slow:
+                    slowPercent = GameManager.Instance.slowPercent;
+                    slowRemainingDuration = GameManager.Instance.slowDuration;
 
-                break;
-            case AdditionalEffect.Poison:
-                poisonDamage = GameManager.Instance.poisonDamage;
-                poisonRemainingDuration = GameManager.Instance.poisonDuration;
-                break;
-            case AdditionalEffect.Stun:
-                freezeRemainingDuration = GameManager.Instance.freezeDuration;
 
-                break;
-            default:
-                break;
+                    break;
+                case AdditionalEffect.Poison:
+                    poisonDamage = GameManager.Instance.poisonDamage;
+                    poisonRemainingDuration = GameManager.Instance.poisonDuration;
+                    GetComponentInChildren<Animator>().Play("Poisoned");
+
+                    break;
+                case AdditionalEffect.Stun:
+                    freezeRemainingDuration = GameManager.Instance.freezeDuration;
+                    GetComponentInChildren<Animator>().Play("Frozen");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
 
     
-
+    
     public void Die()
+    {
+        isAlive = false;
+        GetComponentInChildren<Animator>().Play("Die");
+
+    }
+
+    public void RemoveHim()
     {
         Destroy(this.gameObject);
         GameManager.Instance.currentEnemies.Remove(this);
@@ -91,23 +128,49 @@ public class EnemyBehavior : MonoBehaviour
 
     public void Kamikaze()
     {
-        GameManager.Instance.baseHP--;
-        Destroy(this.gameObject);
-        GameManager.Instance.currentEnemies.Remove(this);
+        if (isAlive)
+        {
+            GameManager.Instance.baseHP--;
+            Destroy(this.gameObject);
+            GameManager.Instance.currentEnemies.Remove(this);
+        }
     }
 
+    bool isAlive = true;
 
     void FixedUpdate()
     {
-        poisonRemainingDuration -= 0.02f;
-        freezeRemainingDuration -= 0.02f;
-        slowRemainingDuration -= 0.02f;
+        if(isAlive)
+        {
+            poisonRemainingDuration -= 0.02f;
+            freezeRemainingDuration -= 0.02f;
+            slowRemainingDuration -= 0.02f;
 
-        if (poisonRemainingDuration >= 0)
-            TakeDamage(poisonDamage);
+            if(slowRemainingDuration <0)
+            {
+                slowPercent = 0;
+            }
 
-        if (freezeRemainingDuration <= 0)
-            transform.position += new Vector3(0, 0, ( 1-slowPercent) * GameManager.Instance.mobSpeedMultiplier * movementspeed/50);
+            if (poisonRemainingDuration >= 0)
+            {
+                GetComponentInChildren<Animator>().SetBool("poisoned", true);
+                if(poisonRemainingDuration % 10 == 0)
+                {
+                    TakeDamage(poisonDamage);
+                }
+            }
+            else
+            {
+                GetComponentInChildren<Animator>().SetBool("poisoned", false);
+            }
+
+            if (freezeRemainingDuration <= 0)
+            {
+                
+                transform.position += new Vector3(0, 0, (1 - slowPercent) * GameManager.Instance.mobSpeedMultiplier * movementspeed / 50);
+            }
+        }
+        
         
     }
 
